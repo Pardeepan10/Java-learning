ccc6d22e94e1ae6077050815667ba93dfe328632
Pardeepan10 <a.p.muthupardeepan@gmail.com>
2025-05-13 20:49:22 +0530

    basic prgram

---
+++ sum.java
@@ -0,0 +1,54 @@
+//count a number
+/*
+import java.util.Scanner;
+public class sum {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+       int count =0;
+       while(n>0){
+           int lastdigt=n%10;
+           count=count+1;
+       n=n/10;
+       }
+        System.out.println(count);
+    }
+}*/
+//reverse a number
+import java.util.Scanner;
+/*
+public class sum {
+    public static void main(String[] args) {
+        int n=8765;
+        int rev=0;
+        while(n>0){
+            int lastdigit=n%10;
+
+            n=n/10;
+            rev=(rev*10)+lastdigit;
+        }
+        System.out.println(rev);
+
+    }
+}*/
+
+//palindrome of number;
+/*ssssssssssss
+public class sum{
+    public static void main(String[] args) {
+        int n=321;
+        int rev=0;
+        int dup=n;
+        while(n>0){
+            int lastdigit=n%10;
+            n=n/10;
+            rev=(rev*10)+lastdigit;
+        }
+        if(dup==rev){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println(" Not an palindrome");
+        }
+    }
+}*/
