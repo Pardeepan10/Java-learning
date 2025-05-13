7d6b55ca573a05c6be7688fd267d18a23b3cee66
Pardeepan10 <a.p.muthupardeepan@gmail.com>
2025-05-13 20:48:02 +0530

    string

---
+++ string.java
@@ -0,0 +1,50 @@
+//reverse a string
+/*public class string {
+    public static void main(String[] args) {
+        String par="pardeepan";
+        int p=par.length();
+
+        for(int i=0;i<p;i++){
+            System.out.print(i);
+            System.out.print(par.charAt(par.length()-i-1));
+        }
+
+    }
+}
+ // replace string
+public class string {
+    public static void main(String[] args) {
+        String s = "g()(al)";
+        String replaced=s.replace("()","o");
+         replaced=replaced.replace("(al)","al");
+        System.out.println(replaced);
+    }
+}*/
+//public class string {
+//    public static void main(String[] args) {
+//        String s = "A man, a plan, a canal: Panama";
+//        String par = s.toLowerCase();
+//        par = par.replaceAll(",", "");
+//        par = par.replaceAll(":", "");
+//        par = par.replaceAll("\\s", "");
+//        System.out.println(par);
+//        char[] word=par.toCharArray();
+//        int start=0;
+//        int end=par.length()-1;
+//        char temp=0;
+//        while(start<end){
+//            temp=word[start];
+//            word[start]=word[end];
+//            word[end]=temp;
+//            start++;
+//            end--;
+//        }
+//        System.out.println(word);
+//        if(par.equals()){
+//            System.out.println("true");
+//        }
+//        else{
+//            System.out.println("false");
+//        }
+//        }
+//    }
